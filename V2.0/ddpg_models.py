9c3bc4b767ee4718b845a6ce9b0bcfc87e2891c9
Jae-Min Cho <81763871+JaeMin98@users.noreply.github.com>
2024-07-17 17:11:46 +0900

    .
    .

--- V2.0/ddpg_models.py
+++ V2.0/ddpg_models.py
@@ -45,7 +45,8 @@ class Actor(nn.Module):
         """Build an actor (policy) network that maps states -> actions."""
         x = F.relu(self.fc1(state))
         x = F.relu(self.fc2(x))
-        return F.tanh(self.fc3(x))
+        x = F.relu(self.fc3(x))
+        return F.tanh(self.fc4(x))
 
 
 class Critic(nn.Module):
